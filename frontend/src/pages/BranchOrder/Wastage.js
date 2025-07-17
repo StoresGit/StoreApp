f1c3b97a315185fae879c0aed392601cac9c3fe5
MuhammadHanzala082 <muhammad.hanzala@pinnacloid.org>
2025-07-17 23:16:41 +0500

    coplete till inventory control

--- frontend/src/pages/BranchOrder/Wastage.js
+++ frontend/src/pages/BranchOrder/Wastage.js
@@ -132,11 +132,7 @@ const Wastage = () => {
           </button>
         </div>
 
-        {/* {error && (
-          <div className="mb-6 p-4 bg-red-100 border border-red-400 text-red-700 rounded">
-            {error}
-          </div>
-        )} */}
+
 
         {/* Create Wastage Form */}
         {showCreateForm && (
